ccc4df98853752274e300ae00624483fc4d91470
mutharasut-cognizant-com <t.mutharasu@cognizant.com>
2017-12-05 10:56:38 -0700

    Oauth and sso changes

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -38,6 +38,7 @@ public class ApplicationServer {
         start(envMapBuilder()
                 .put("SPRING_DATASOURCE_URL", dbUrl)
                 .put("REGISTRATION_SERVER_ENDPOINT", "http://registration-server")
+                .put("APPLICATION_OAUTH_ENABLED", "false")
                 .build()
         );
     }
